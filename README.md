b274a490426700e012d2d71a663c1fa1fa6703e1
Oliver Scheer <oscheer@microsoft.com>
2018-03-28 16:38:55 +0200

    fine tuning

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
 # send-email-with-device-ip-address
+
 For headless devices it is hard to be discovered. This small script is fixing the issue, that you don't see the ip-address of your device. 
+
+## To Do's
+
+Create a file called 'secrets.py' and add your personal settings to connect to a mail server. 
+
+``` Python
+
+sender_address = "yourname@gmail.com"
+sender_password = "your password"
+sender_server = 'smtp.gmail.com'
+sender_port = 587
+recipient_address = "yourrecipient@outlookc.com"
+
+``` 
+
+check if crontab is installed on your device. 
+if not, check that tutorial https://www.raspberrypi.org/documentation/linux/usage/cron.md
+
+
+## Running it on every start of device
+
+Currently I only this tool on Raspberry Pi's based on Raspbian. So this is the steps you need to install it as startup app. 
+
+``` bash
+chmod 755 run.sh
+```
+
+Use crontab to schedule script every hour and on reboot
+
+0 * * * * 
+
+Use The '&' for running in background
+
+@reboot python /home/pi/apps/send-email-with-device-ip-address/main.py & 
+0 * * * * python /home/pi/apps/send-email-with-device-ip-address/main.py & 
+
+To execute the script on every reboot:
+
+``` shell
+sudo nano /etc/rc.local 
+``` 
+And add the following line to it. The command sleep 30 is necessary to avoid an , because it takes a while to get an valid Ip-address on startup:
+
+``` shell
+(sleep 15; python /home/pi/apps/send-email-with-device-ip-address/main.py)&
+```
+
+## Feedback is always welcome
+
+If you like to send me comments, feel free to comment.
--- main.py
+++ main.py
@@ -3,38 +3,54 @@
 import socket
 import smtplib
 import os
+import sys
 
 # Create a secrets.py file and place the following variables 
 # sender_address = "<sender email address here>"
 # sender_password = "<sender passwort here>"
+# sender_server = 'smtp.gmail.com'
+# sender_port = 587
 # recipient_address = "<recipient address here>"
 
 from secrets import sender_address
 from secrets import sender_password
+from secrets import sender_server
+from secrets import sender_port
 from secrets import recipient_address 
 
-
 def get_device_ip_address():
-    # On Windows
-    # hostname = socket.gethostname()
-    # result = "Hostname:  " + hostname
-    # host = socket.gethostbyname(hostname)
-    # result += "\nHost-IP-Address:" + host
 
-    gw = os.popen("ip -4 route show default").read().split()
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.connect((gw[2], 0))
-    ipaddr = s.getsockname()[0]
-    gateway = gw[2]
-    host = socket.gethostname()
-    result = "IP:" + ipaddr + " Gateway:" + gateway + " Host: " + host
-    return result
+    try: 
+        if os.name == "nt":
+            # On Windows
+            result = "Running on Windows"
+            hostname = socket.gethostname()
+            result += "\nHostname:  " + hostname
+            host = socket.gethostbyname(hostname)
+            result += "\nHost-IP-Address:" + host
+            return result
+
+        elif os.name == "posix":
+            gw = os.popen("ip -4 route show default").read().split()
+            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            s.connect((gw[2], 0))
+            ipaddr = s.getsockname()[0]
+            gateway = gw[2]
+            host = socket.gethostname()
+            result = "OS:\t\tRaspbian\nIP:\t\t" + ipaddr + "\nGateway:\t" + gateway + "\nHost:\t\t" + host
+            return result
+        
+        else:
+            result = os.name + " not supported yet."
+            return result
+    except:
+        return "Could not detect ip address"
 
 def send_email(text):
     try:
-        message = "\"From: " + sender_address + "\nTo: " + recipient_address + "\nSubject: Device Information\n\n" + text + "\""
+        message = "From: " + sender_address + "\nTo: " + recipient_address + "\nSubject: Device Information\n\n" + text 
 
-        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server = smtplib.SMTP(sender_server, sender_port)
         server.ehlo()
         server.starttls()
         server.login(sender_address, sender_password)
@@ -46,8 +62,9 @@ def send_email(text):
         print("failed to send email")
 
 message = get_device_ip_address()
-print("device data:")
 print(message)
+print("Sending email, can take a while.")
 send_email(message)
 print("Done.")
 
+sys.exit()
